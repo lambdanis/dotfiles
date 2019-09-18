b236a8487af5ec43455402ee9c2000e34439d82a
Ania Kapuścińska <ania0102@gmail.com>
2019-09-18 17:21:32 +0100

    Add rename_jsons.sh scrript

---
+++ rename_jsons.sh
@@ -0,0 +1,14 @@
+# Rename a batch of json files using values of a json field
+
+directory="$1"  # must not have a slash at the end
+field_in_json="id"
+suffix="-renamed"
+
+for file in $directory/*.json; do
+    # If jq fails, an error will be printed, but the file still will be renamed to $suffix.json
+    # In case of multiple failures, it will be overriden and other files may be lost.
+    # FIXME: Handle jq errors
+    new_name=$(jq -r ".$field_in_json" "$file");
+    mv "$file" "$directory/$new_name$suffix.json"
+done
+
